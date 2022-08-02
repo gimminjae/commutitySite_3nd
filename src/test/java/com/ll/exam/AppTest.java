ba1f29ee3bd095cfd81e26216c1e70010ec10cd2
gimminjae <min356812@naver.com>
2022-08-02 10:27:08 +0900

    articleController와 같이 1개만 필요한 것은 싱글톤으로 작동

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -14,8 +14,15 @@ public class AppTest {
     }
     @Test
     public void ioc__articleController() {
-        ArticleController articleController = Con.getArticleController();
+        ArticleController articleController = Container.getArticleController();
 
         assertThat(articleController).isNotNull();
+    }
+    @Test
+    public void ioc__articleController__싱글톤() {
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController1).isEqualTo(articleController2);
     }
 }
